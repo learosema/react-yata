a16bb90814035b5df0f164087b17ec1a54b65c5f
Lea Rosema <lea.rosema@sinnerschrader.com>
2019-05-15 22:34:52 +0200

    Updates

--- src.78399e21.js
+++ src.78399e21.js
@@ -32335,7 +32335,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59870" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62490" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
